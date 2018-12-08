156e7c851e769a9edc4e0a4fd7a527333c295b00
Rahul <rahulreghunath11@gmail.com>
2018-12-08 16:43:37 +0530

    Added option to highlight parent element

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 # active-link
 
 
-#### Tiny js library to highlight active page links in web page
+#### Tiny js library to highlight active page links in web page by adding given class to the element
 
 
 ### Install
@@ -28,7 +28,8 @@ highlight({options})
 | Name      | Required | Type          | Default     | Description |
 | ---       | ---      | ---           | ---         | ---         |
 | activeClass | false  | String | active | Class to added to active link element |
-| container | true  | String | none| Element's name, #id or .class in which contain the links to be highlighted. Element must be unique |
+| container | true  | String | none| Element's name, #id or .class in which contain the links to be highlighted. Element must be unique. |
+| highlightParent | false  | Boolean | false | To highlight parent element of link |
 
 ---
 
--- src/active-link.js
+++ src/active-link.js
@@ -1,12 +1,26 @@
-let highlight = ({activeClass = 'active', container,}) => {
+let highlight = ({activeClass = 'active', container, highlightParent = false}) => {
 
-    let links = document.querySelector(container).querySelectorAll("a");
+    let links;
+
+    try {
+        links = document.querySelector(container).querySelectorAll("a");
+    } catch (e) {
+        throw ('container element not give');
+    }
 
     for (const key of links) {
         if (key.getAttribute('href').includes('http') && key.getAttribute('href') === window.location.href) {
-            key.setAttribute('class', activeClass);
+            if (highlightParent) {
+                key.parentElement.setAttribute('class', activeClass);
+            } else {
+                key.setAttribute('class', activeClass);
+            }
         } else if (window.location.href.includes(key.getAttribute('href'))) {
-            key.setAttribute('class', activeClass);
+            if (highlightParent) {
+                key.parentElement.setAttribute('class', activeClass);
+            } else {
+                key.setAttribute('class', activeClass);
+            }
         }
     }
 };
